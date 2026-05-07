bebe4271502d463bde8b66cd59d163f2a631d16e
lipcoder <lipcoder@foxmail.com>
2026-05-07 19:18:44 +0800

    add user action

---
+++ internal/service/mainAction.go
@@ -0,0 +1,117 @@
+package service
+
+import (
+	"context"
+	"errors"
+	"lipcoder/face/internal/camera"
+	"lipcoder/face/internal/recognition"
+	"strings"
+)
+
+type AdminRequest struct {
+	name   string
+	action string
+	cam    camera.Camera
+	rec    recognition.Recognition
+	Reply  chan AdminResult
+}
+
+type AdminResult struct {
+	action string
+	name   string
+	exists bool
+	err    error
+}
+
+func StartAdminLoop(
+	ctx context.Context,
+	reqCh <-chan AdminRequest,
+	addFaceSem chan struct{},
+) error {
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case req, ok := <-reqCh:
+			if !ok {
+				return nil
+			}
+			req.name = strings.TrimSpace(req.name)
+			if req.name == "" {
+				return errors.New("name cannot be empty")
+			}
+			if req.cam == nil {
+				return errors.New("camera cannot be nil")
+			}
+
+			switch req.action {
+			case "add":
+				go handleAddFaceRequest(ctx, req, addFaceSem)
+
+			case "delete":
+				exists, err := DeleteFace(ctx, req.name)
+				sendAdminResult(ctx, req.Reply, AdminResult{
+					name:   req.name,
+					action: req.action,
+					exists: exists,
+					err:    err,
+				})
+
+			case "search":
+				exists, err := QueryFace(ctx, req.name)
+				sendAdminResult(ctx, req.Reply, AdminResult{
+					name:   req.name,
+					action: req.action,
+					exists: exists,
+					err:    err,
+				})
+			}
+		}
+	}
+}
+
+// 管理addFace的并发数量
+func handleAddFaceRequest(
+	ctx context.Context,
+	req AdminRequest,
+	addFaceSem chan struct{},
+) {
+	select {
+	case <-ctx.Done():
+		sendAdminResult(ctx, req.Reply, AdminResult{
+			name:   req.name,
+			action: req.action,
+			err:    ctx.Err(),
+		})
+		return
+	case addFaceSem <- struct{}{}:
+		defer func() {
+			<-addFaceSem
+		}()
+	}
+	_, err := AddFaceFromCamera(ctx, req.name, req.cam, req.rec)
+	sendAdminResult(ctx, req.Reply, AdminResult{
+		name:   req.name,
+		action: req.action,
+		err:    err,
+	})
+}
+
+// 返回请求
+func sendAdminResult(
+	ctx context.Context,
+	reply chan AdminResult,
+	result AdminResult,
+) {
+	if reply == nil {
+		return
+	}
+
+	select {
+	case <-ctx.Done():
+		return
+
+	case reply <- result:
+		return
+	}
+}
---
+++ internal/service/tools.go
@@ -0,0 +1,85 @@
+package service
+
+import (
+	"context"
+	"fmt"
+	"lipcoder/face/internal/camera"
+	facedb "lipcoder/face/internal/data"
+	"lipcoder/face/internal/recognition"
+)
+
+func AddFaceFromCamera(
+	ctx context.Context,
+	name string,
+	cam camera.Camera,
+	rec recognition.Recognition,
+) (int64, error) {
+	exists, err := QueryFace(ctx, name)
+	if err != nil {
+		return 0, fmt.Errorf("add face false %w", err)
+	}
+	if exists {
+		return 0, nil
+	}
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	default:
+	}
+
+	imageBytes, err := cam.Capture()
+	if err != nil {
+		return 0, fmt.Errorf("mainAction get image failed,%w", err)
+	}
+
+	select {
+	case <-ctx.Done():
+		return 0, ctx.Err()
+	default:
+	}
+
+	embedding, err := rec.GetFaceEmbedding(imageBytes, 0)
+	if err != nil && embedding != nil {
+		return 0, fmt.Errorf("get embedding from inspireface response: %w", err)
+	} else if err != nil && embedding == nil {
+		return 0, fmt.Errorf("chect face counts %w", err)
+	}
+
+	embeddingText := rec.EmbeddingToPGVector(embedding[0])
+
+	id, err := facedb.AddFace(ctx, name, embeddingText)
+	if err != nil {
+		return 0, fmt.Errorf("add face to database: %w", err)
+	}
+
+	return id, nil
+}
+
+func DeleteFace(
+	ctx context.Context,
+	name string,
+) (bool, error) {
+	exists, err := QueryFace(ctx, name)
+	if err != nil {
+		return false, fmt.Errorf("delete face false %w", err)
+	}
+	if !exists {
+		return false, nil
+	}
+
+	if err := facedb.DeleteFaceByName(ctx, name); err != nil {
+		return false, fmt.Errorf("delete face by name: %w", err)
+	}
+	return true, nil
+}
+
+func QueryFace(
+	ctx context.Context,
+	name string,
+) (bool, error) {
+	exists, err := facedb.FaceExistsByName(ctx, name)
+	if err != nil {
+		return false, fmt.Errorf("query face by name: %w", err)
+	}
+	return exists, nil
+}
